41f313a87c525bd5461ce24244a17b69f96c8f34
Evelyn Maguire <77428889+evelyn-fyi@users.noreply.github.com>
2024-06-01 18:05:06 -0600

    commit asset map changes, update name on transfer asset classes, small mods to transfer asset lwc (#26)

---
+++ force-app/chapter02/default/lwc/assetMap/assetMap.js
@@ -0,0 +1,37 @@
+import { LightningElement, api, wire } from 'lwc';
+import getMapMarkers from '@salesforce/apex/AssetMapInterface.getMapMarkers';
+import { RefreshEvent } from 'lightning/refresh';
+import{ refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+export default class AssetMap extends LightningElement {
+    @api recordId;
+    _wiredMarkers;
+    mapMarkers;
+
+    @wire(getMapMarkers, {assetId : '$recordId'})
+    wiredMapMarkers(result) {
+        this._wiredMarkers = result;
+        if (result.data) {
+            this.mapMarkers = JSON.parse(result.data);
+            this.error = undefined;
+        } else if(result.error) {
+            this.error = result.error;
+            this.mapMarkers = undefined;
+            this.showNotif('There has been an error!', this.error, 'error');
+        }
+    }
+
+    refreshMap() {
+        return refreshApex(this._wiredMarkers);
+    }
+
+    showNotif(title, msg, variant) {
+        const evt = new ShowToastEvent({
+          title: title,
+          message: msg,
+          variant: variant,
+        });
+        this.dispatchEvent(evt);
+    }
+}
