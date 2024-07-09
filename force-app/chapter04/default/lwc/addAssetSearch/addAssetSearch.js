88fededb6fcc147e49ceaa5c2fb2cb037c0cd22e
Evelyn Maguire <me@evelyn.fyi>
2024-07-09 06:31:51 -0600

    rearrange ch04 and ch05

--- force-app/chapter04/default/lwc/addAssetSearch/addAssetSearch.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class AddAssetSearch extends LightningElement {
-    @api searchTerm;    
-
-    handleChange(event) {
-        this.searchTerm = event.detail.value;
-    }
-}
