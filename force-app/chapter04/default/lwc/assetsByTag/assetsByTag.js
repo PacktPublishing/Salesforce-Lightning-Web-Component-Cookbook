c94a7ac28a6b7eae330d7200b69d3ce0ff2fe89a
Evelyn Maguire <me@evelyn.fyi>
2024-10-30 12:39:56 -0600

    remove offset and limit from query for lazy loading

--- force-app/chapter04/default/lwc/assetsByTag/assetsByTag.js
+++ force-app/chapter04/default/lwc/assetsByTag/assetsByTag.js
@@ -32,7 +32,7 @@ export default class AssetsByTag extends LightningElement {
 
     async getTagsWithAssets() {
         try{
-            const tagsWithAssets = await returnTagsWithAssetsByAccount({ accountIdString : this.accountId, lim: this.limit, offset: this.offset });
+            const tagsWithAssets = await returnTagsWithAssetsByAccount({ accountIdString : this.accountId });
 
             this._tagsWithAssetsResult = this.dataFlattener(tagsWithAssets);
 
@@ -43,7 +43,7 @@ export default class AssetsByTag extends LightningElement {
     }
 
     getTagsWithAssetsPromise() {
-        returnTagsWithAssetsByAccount({ accountIdString : this.accountId, lim: this.limit, offset: this.offset })
+        returnTagsWithAssetsByAccount({ accountIdString : this.accountId })
             .then(tagsWithAssets => {
                 this._tagsWithAssetsPromiseResult = this.dataFlattener(tagsWithAssets);
 
@@ -87,7 +87,7 @@ export default class AssetsByTag extends LightningElement {
 
     async getTagsWithAssetsWrappers() {
         try{
-            const tagsWithAssetWrappers = await returnTagsWithAssetsWrappersByAccount({ accountIdString : this.accountId, lim: this.limit, offset: this.offset });
+            const tagsWithAssetWrappers = await returnTagsWithAssetsWrappersByAccount({ accountIdString : this.accountId });
 
             let wrappersToSort = [...tagsWithAssetWrappers];
 
