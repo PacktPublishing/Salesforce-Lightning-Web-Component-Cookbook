a82b498d31c3a108e021e4444f8ce197c99cfce6
Evelyn Maguire <me@evelyn.fyi>
2025-01-01 22:09:26 -0700

    commit flattened DOM structure, flow, verified deployment in new dev org

---
+++ force-app/chapter06/default/lwc/viewContactsFlattened/viewContactsFlattened.js
@@ -0,0 +1,80 @@
+import { LightningElement, api, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import returnContactsPerAccount from '@salesforce/apex/ViewContactController.returnContactsPerAccount';
+import Utilities from 'c/notifUtils';
+let utility;
+
+export default class ViewEditContactsFlattened extends NavigationMixin(LightningElement) {
+    contactColumnsDatatable = [
+        { label: 'First Name', fieldName: 'FirstName' },
+        { label: 'Last Name', fieldName: 'LastName' },
+        { label: 'Title', fieldName: 'Title' }
+    ];
+
+    @api recordId;
+    contactsToView;
+    _wiredContacts;
+    columns;
+    selectedContact = '';
+    addEventInactive = true;
+
+    get flowInputVariables() {
+        return [
+            {
+                name: 'AccountId',
+                type: 'String',
+                value: this.recordId
+            },
+            {
+                name: 'ContactId',
+                type: 'String',
+                value: this.selectedContact
+            }
+        ];
+    }
+
+    connectedCallback() {
+        utility = new Utilities(this);
+    }
+
+    @wire(returnContactsPerAccount, {accountIdString : '$recordId'})
+    wiredContacts(result) {
+        this._wiredContacts = result;
+        if (result.data) {
+            this.contactsToView = result.data;
+        } else if(result.error) {
+            this.contactsToView = undefined;
+            utility.showNotif('There has been an error returning contacts!', result.error, 'error');
+        }
+    }
+
+    handleContactSelect(event) {
+        this.selectedContact = event.detail.selectedRows[0].Id;
+    }
+
+    handleAddEvent() {
+        this.addEventInactive = false;
+    }
+
+    handleFlowStatusChange(event) {
+        if (event.detail.status === 'FINISHED') {
+            this.selectedContact = '';
+            this.addEventInactive = true;
+
+            this.refreshPage();
+
+            utility.showNotif('Your event has been inserted successfully!', event.detail.outputVariables[0].value, 'success');
+        }
+    }
+
+    refreshPage() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: 'Account',
+                actionName: 'view'
+            }
+        });
+    }
+}
