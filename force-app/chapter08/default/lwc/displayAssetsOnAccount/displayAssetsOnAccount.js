ea66c0f89906588b2681cbe3a1eabd7272db047d
Evelyn Maguire <me@evelyn.fyi>
2025-01-13 22:10:29 -0700

    fix lazy loading for ch8

--- force-app/chapter08/default/lwc/displayAssetsOnAccount/displayAssetsOnAccount.js
+++ force-app/chapter08/default/lwc/displayAssetsOnAccount/displayAssetsOnAccount.js
@@ -10,8 +10,7 @@ export default class DisplayAssetsOnAccount extends LightningElement {
     @api recordId
     isLoading = true;
     scrollingAssets = [];
-    allAssets = [];
-    assetsForDatatable;
+    assetsForDatatable = [];
     preselectedRows = [];
     _wiredAssets;
     offset = 0;
@@ -22,51 +21,42 @@ export default class DisplayAssetsOnAccount extends LightningElement {
         return WIDE_COLUMNS_DEFINITION;
     }
 
-    connectedCallback() {
+    async connectedCallback() {
         utility = new Utilities(this);
+        try {
+            const returnedAssets = await returnAssetsByAccount({ accountIdString : this.recordId, lim : this.limit, offset : this.offset });
+            let tempAssets = JSON.parse(JSON.stringify(returnedAssets));
+
+            this.assetsForDatatable = this.formatAssets(tempAssets);
+
+            let firstRecord = this.assetsForDatatable[0];
+            
+            this.preselectedRows = [firstRecord.Id];
+            this.scrollingAssets = new Set([firstRecord, ...this.scrollingAssets]);
+
+            this.error = undefined;        
+            this.loading = false;
+        } catch (error) {
+            this.error = error;
+            utility.showNotif('There has been an error loading assets!', this.error.message, 'error');
+        }
     }
 
     @wire(returnAssetCount, { accountIdString : '$recordId'})
     totalAssets;
 
-    @wire(returnAssetsByAccount, { accountIdString : '$recordId', lim : '$limit', offset : '$offset'})
-    wiredAccounts(result) {
-        this._wiredAssets = result;
-        if (result.data) {
-            let tempAssets = JSON.parse(JSON.stringify(result.data));
-
-            this.allAssets = this.formatAssets(tempAssets);
-            this.assetsForDatatable = this.allAssets;
-
-            this.preselectRows();
-
-            this.error = undefined;
-            this.loading = false;
-        } else if(result.error) {
-            this.error = result.error;
-            this.scrollingAssets = undefined;
-            this.assetsForDatatable = undefined;
-            utility.showNotif('There has been an error loading assets!', this.error, 'error');
-            this.loading = false;
-        }
-    }
-
     formatAssets(tempAssets) {
         tempAssets.forEach(asset => {
             if(!asset.hasOwnProperty('Primary_Image_Small__c')) {
                 asset['Primary_Image_Small__c'] = NO_IMAGE_FOUND;
+
+                if(!asset.Is_Public_Domain__c) {
+                    asset['Description'] = 'This image is not in the public domain, so it cannot be displayed here.';
+                }
             }
-            if(!asset.Is_Public_Domain__c) {
-                asset['Description'] = 'This image is not in the public domain, so it cannot be displayed here.';
-            }
-            this.allAssets.push(asset);
         });
 
         return tempAssets;
-    }
-
-    preselectRows() {
-        this.preselectedRows = [this.assetsForDatatable[0].Id];
     }
 
     updateCarousel(event) {
@@ -75,32 +65,25 @@ export default class DisplayAssetsOnAccount extends LightningElement {
         let selectedRows = event.detail.selectedRows;
         this.preselectedRows = [selectedRows[0].Id];
 
-        console.log('selectedRows: ' + JSON.stringify(selectedRows));
         this.scrollingAssets = new Set([...selectedRows, ...this.scrollingAssets]);
 
         this.isLoading = false;
     }
 
     async loadMoreAssets() {
-        console.log('totalAssets: ' + JSON.stringify(this.totalAssets));
         if(this.offset < this.totalAssets.data) {
             try {
                 this.isLoading = true;
                 this.offset += this.limit;
         
-                const nextAssets = await returnAssetsByAccount({ accountIdString : this.recordId, lim : this.limit, offset : this.offset });
+                const returnedAssets = await returnAssetsByAccount({ accountIdString : this.recordId, lim : this.limit, offset : this.offset });
+                let tempAssets = JSON.parse(JSON.stringify(returnedAssets));
 
-                console.log('nextAssets: ' + nextAssets);
-
-                // let tempAssets = JSON.parse(JSON.stringify(nextAssets));
-
-                // let assetsToAdd = this.formatAssets(tempAssets);
-        
-                // this.allAssets = [...this.allAssets, ...assetsToAdd];
-                // this.assetsForDatatable = this.allAssets;
-        
-                // this.isLoading = false;    
-            } catch(error) {
+                this.assetsForDatatable = [...this.assetsForDatatable, ...this.formatAssets(tempAssets)];
+    
+                this.error = undefined;
+                this.loading = false;
+            } catch (error) {
                 this.error = error;
                 utility.showNotif('There has been an error loading more assets!', this.error.message, 'error');
             }    
