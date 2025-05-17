e858b2f91955b675f6e71b736251dfff7809a486
Evelyn Maguire <me@evelyn.fyi>
2025-05-16 22:45:09 -0700

    slight refactor

--- force-app/chapter10/default/lwc/polymorphicDropdown/polymorphicDropdown.js
+++ force-app/chapter10/default/lwc/polymorphicDropdown/polymorphicDropdown.js
@@ -3,14 +3,12 @@ import { LightningElement, api } from 'lwc';
 export default class SearchableDropdown extends LightningElement {
     @api dropdownOptions;
 
-    selectedOption;
-
     selectOption(event) {
-        this.selectedOption = event.currentTarget.dataset.id;
+        let selectedOption = event.currentTarget.dataset.id;
 
         let tempOptions = this.dropdownOptions;
 
-        let selectedElement = tempOptions.filter(option => option.value === this.selectedOption);
+        let selectedElement = tempOptions.filter(option => option.value === selectedOption);
 
         const setSelectionEvent = new CustomEvent('setselection', {
             bubbles : false,
