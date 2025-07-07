cd79d11d9c35a56801ea07bb90700ff1970f4149
Evelyn Maguire <me@evelyn.fyi>
2025-07-07 05:08:42 -0600

    chapter 7 code

---
+++ force-app/chapter07/default/lwc/changeRequestLauncher/changeRequestLauncher.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc';
+import changeRequestModal from 'c/changeRequestModal';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class ChangeRequestLauncher extends NavigationMixin(LightningElement) {
+    @api recordId;
+
+    @api invoke() {
+        changeRequestModal.open({
+            size: 'large',
+            description: 'A modal to insert a new Change Request for the specified Asset.',
+            content: {recordId : this.recordId}
+        }).then((result) => {
+            this.handleSaveSuccess(result.navId);
+        })
+    }
+
+    handleSaveSuccess(id) {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: id,
+                objectApiName: 'ChangeRequest',
+                actionName: 'view'
+            }
+        });
+    }
+}
