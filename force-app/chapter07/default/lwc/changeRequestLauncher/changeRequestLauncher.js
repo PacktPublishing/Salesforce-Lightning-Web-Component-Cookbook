20c4013cd5c293c7414d478ff99b5c178b621e12
Evelyn Maguire <me@evelyn.fyi>
2025-10-15 01:29:18 -0600

    commit lws enabler permissions for modest feature flag

--- force-app/chapter07/default/lwc/changeRequestLauncher/changeRequestLauncher.js
+++ force-app/chapter07/default/lwc/changeRequestLauncher/changeRequestLauncher.js
@@ -1,34 +1,39 @@
 import { LightningElement, api } from 'lwc';
+import lwsEnabled from "@salesforce/customPermission/LWS_Enabler";
 import changeRequestModal from 'c/changeRequestModal';
-// import changeRequestModalLws from 'c/changeRequestModalLws';
+import changeRequestModalLws from 'c/changeRequestModalLws';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class ChangeRequestLauncher extends NavigationMixin(LightningElement) {
     @api recordId;
 
     @api invoke() {
-        changeRequestModal.open({
-            size: 'large',
-            description: 'A modal to insert a new Change Request for the specified Asset.',
-            content: {recordId : this.recordId}
-        }).then((result) => {
-            if(result) {
-                this.handleSaveSuccess(result.navId);
-            }
-        })
-    }
+        if(lwsEnabled) {
+            console.log('LWS Permission Set Assigned');
 
-    // @api invoke() {
-    //     const result = changeRequestModalLws.open({
-    //         size: 'large',
-    //         description: 'A modal to insert a new Change Request for the specified Asset.',
-    //         content: {recordId : this.recordId},
-    //         onrecordcreated: (event) => {
-    //             event.stopPropagation();
-    //             this.handleSaveSuccess(event.detail);
-    //         }
-    //     })
-    // }
+            const result = changeRequestModalLws.open({
+                size: 'large',
+                description: 'An LWS only modal to insert a new Change Request for the specified Asset.',
+                content: {recordId : this.recordId},
+                onrecordcreated: (event) => {
+                    event.stopPropagation();
+                    this.handleSaveSuccess(event.detail);
+                }
+            })
+        } else {
+            console.log('Lightning Locker Compatible');
+
+            changeRequestModal.open({
+                size: 'large',
+                description: 'A modal to insert a new Change Request for the specified Asset.',
+                content: {recordId : this.recordId}
+            }).then((result) => {
+                if(result) {
+                    this.handleSaveSuccess(result.navId);
+                }
+            })
+        }
+    }
 
     handleSaveSuccess(id) {
         this[NavigationMixin.Navigate]({
