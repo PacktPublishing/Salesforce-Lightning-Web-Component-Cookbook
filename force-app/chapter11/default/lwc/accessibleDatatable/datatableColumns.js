5b8ad4367d4f5bf56adb26cabf1621a97df90281
Evelyn Maguire <me@evelyn.fyi>
2025-12-15 02:24:18 -0700

    rework ch11 code: accessible vs inaccessible tables

---
+++ force-app/chapter11/default/lwc/accessibleDatatable/datatableColumns.js
@@ -0,0 +1,20 @@
+export const COLUMNS_DEFINITION = [
+    {
+        type: 'text',
+        fieldName: 'Name',
+        label: 'Name',
+        hideDefaultActions: true
+    },
+    {
+        type: 'text',
+        fieldName: 'Phone',
+        label: 'Phone',
+        hideDefaultActions: true
+    },
+    {
+        type: 'text',
+        fieldName: 'AnnualRevenue',
+        label: 'Annual Revenue',
+        hideDefaultActions: true
+    },
+]
